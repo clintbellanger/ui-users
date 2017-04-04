eacc53e56911915bfeeced40c0e326b27168a956
Mike Taylor <mike@indexdata.com>
2017-04-04 13:50:33 +0100

    Lint fixes

--- settings/PermissionSets.js
+++ settings/PermissionSets.js
@@ -1,3 +1,5 @@
+// We have to remove node_modules/react to avoid having multiple copies loaded.
+// eslint-disable-next-line import/no-unresolved
 import React from 'react';
 
 import Paneset from '@folio/stripes-components/lib/Paneset';
